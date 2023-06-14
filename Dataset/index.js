004f07a0dc02efe582c8d5617de095eab9c1ce72
Audy Revi Nugraha <audyrev@gmail.com>
2023-06-14 17:20:14 +0700

    update

--- Dataset/index.js
+++ Dataset/index.js
@@ -1,23 +1,24 @@
-const {loadJabar, loadJateng, loadSumsel, firstaid} = require('./user_model');
+const {loadJabar, loadJateng, loadSumsel, firstaid} = require('./user_model.js');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 'isi port anda';
 app.use(express.json());
-app.listen(PORT, () => console.log('Server running...'));
+app.listen(PORT, () =>  console.log(`Server listening on port ${PORT}...`));
 
-app.get('/',(req,res) => res.send('Successful Response'));
+app.get('/',(req,res) => res.status(200).json({msg: 'Successful Response'}));
 
-app.get('/search', (req,res)=>{
+app.post('/search',(req,res)=>{
     const search = req.body.search;
-    if(search == "Jawa Barat" ) { 
-        res.send(loadJabar()); }
+    if(search == "Jawa Barat") { 
+        res.status(200).json(loadJabar()); 
+    }
         else if(search == "Jawa Tengah"){
-            res.send(loadJateng());
+            res.status(200).json(loadJateng());
         } else if(search == "Sumatera Selatan"){
-            res.send(loadSumsel());
-        } else if(search == "First Aid"){
-            res.send(firstaid());
-        }else{
-            res.status(400).json({msg : "Response Unsuccessful"});
+            res.status(200).json(loadSumsel());
+            } else if(search == "First Aid"){
+                res.status(200).json(firstaid());
+                }else{
+                    res.status(400).json({msg : "Response Unsuccessful"});
         }
 });
