9686901c2b2d5d08cc9b24a3a9a4259f9b193732
Audy Revi Nugraha <128171860+audyrevi@users.noreply.github.com>
2023-06-16 13:57:37 +0700

    Update index.js

--- Dataset/index.js
+++ Dataset/index.js
@@ -1,7 +1,7 @@
 const {loadJabar, loadJateng, loadSumsel, firstaid} = require('./user_model.js');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 'isi port anda';
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.listen(PORT, () =>  console.log(`Server listening on port ${PORT}...`));
 
@@ -9,12 +9,12 @@ app.get('/',(req,res) => res.status(200).json({msg: 'Successful Response'}));
 
 app.post('/search',(req,res)=>{
     const search = req.body.search;
-    if(search == "Jawa Barat") { 
+    if(search == "Jawa Barat" || search == "West Java") { 
         res.status(200).json(loadJabar()); 
     }
-        else if(search == "Jawa Tengah"){
+        else if(search == "Jawa Tengah" || search == "Central Java"){
             res.status(200).json(loadJateng());
-        } else if(search == "Sumatera Selatan"){
+        } else if(search == "Sumatera Selatan" || search == "South Sumatera"){
             res.status(200).json(loadSumsel());
             } else if(search == "First Aid"){
                 res.status(200).json(firstaid());
